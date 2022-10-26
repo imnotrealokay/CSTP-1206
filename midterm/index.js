f1b60cf253f6d1f18483a36c72377d7982b6210c
Raghav Chhabra <rghvchh@Gmail.com>
2022-10-25 23:24:15 -0700

    Some progress

--- midterm/index.js
+++ midterm/index.js
@@ -4,14 +4,40 @@ const app = express();
 const port = 5000;
 //const cors = require("cors");
 
+let employeeList = [
+    {
+        id: 1,
+        name: "Mike",
+        email: "mike@company.com",
+        employeeID: 100,
+        department: "IT",
+        salary: 100000
+    },
+    {
+        id: 2,
+        name: "Daniel",
+        email: "daniel@company.com",
+        employeeID: 101,
+        department: "HR",
+        salary: 50000
+    }
+]
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.get("/",(req, res) =>{
+    res.redirect("/employees");
+})
 //app.use(cors());
 
 
 ///1
 app.get("/employees", (req, res) => {
-    res.send("Working!!")
+    return res.status(200).json({
+        message: "succesfull",
+        data: employeeList
+    })
 })
 // app.listen(port, () => {
 //     console.log('Listening at http://localhost:' + port);
@@ -19,18 +45,19 @@ app.get("/employees", (req, res) => {
 
 
 //2
-app.get("/employees/:department",(req, res) => {
+app.get("/employees/id/:department",(req, res) => {
     res.send("Working too!!")
 })
 
 //3
-app.get("/employees/id/:employeeID",(req, res) => {
-    res.send("Working buddy")
+app.get("/employees/:employeeID",(req, res) => {
+    res.send(200)("Done!")
 })
+
 //4
-app.post("/employees", urlencoded, function(req, res){
-    res.render("/employees", {q: req, res})
-})
+// app.post("/employees", urlencoded, function(req, res){
+//     res.render("/employees", {q: req, res})
+// })
 
 
 app.listen(port, () => {
